1beb8060d9918b5ab8049019be5494630f32aa4f
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-01-10 16:48:01 +0100

    feat: aggiunta dettaglio card

--- src/components/pages/PostDetail.jsx
+++ src/components/pages/PostDetail.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function PostDetail() {
@@ -10,19 +10,19 @@ function PostDetail() {
   useEffect(() => {
     axios.get(`${urlIndex}/${id}`).then((Response) => {
       setPost(Response.data);
+      console.log(Response.data);
     });
   }, []);
 
   return (
     <>
-      <div className={post.card}>
-        <h2 className={post.title}>{titolo}</h2>
-        <img src={`${urlIndex}/${immagine}`} alt="" className={post.image} />
-        <p className={post.content}>{contenuto}</p>
-        <h4 className={post.category}>{categoria}</h4>
-        <div className={post.tags}>
-          {arraySpanTags &&
-            arraySpanTags.map((tag) => <span className={post.tag}>{tag}</span>)}
+      <div key={post.id}>
+        <div>
+          <h5>{post.titolo}</h5>
+          {post.immagine && (
+            <img src={`${urlIndex}/${post.immagine}`} alt={post.immagine} />
+          )}
+          <p>{post.contenuto}</p>
         </div>
       </div>
     </>
