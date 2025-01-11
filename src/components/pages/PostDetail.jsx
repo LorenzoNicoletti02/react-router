0ecba42b41b82afacbdc71ce1e69bc3dfce05fb4
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-01-11 16:55:38 +0100

    fix: url immagini corrette nei ditail

--- src/components/pages/PostDetail.jsx
+++ src/components/pages/PostDetail.jsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function PostDetail() {
-  const urlIndex = "http://localhost:3000/posts";
+  const urlIndex = "http://localhost:3000";
   const { id } = useParams();
   const [post, setPost] = useState({});
 
   useEffect(() => {
-    axios.get(`${urlIndex}/${id}`).then((Response) => {
+    axios.get(`${urlIndex}/posts/${id}`).then((Response) => {
       setPost(Response.data);
       console.log(Response.data);
     });
@@ -20,7 +20,10 @@ function PostDetail() {
         <div>
           <h5>{post.titolo}</h5>
           {post.immagine && (
-            <img src={`${urlIndex}/${post.immagine}`} alt={post.immagine} />
+            <img
+              src={`${urlIndex}/${post.immagine}`}
+              alt={`${urlIndex}/${post.immagine}`}
+            />
           )}
           <p>{post.contenuto}</p>
         </div>
